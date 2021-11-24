f5e1bc1043a683819c47f8d82166550e0d575fc9
hiphurraa <61519166+hiphurraa@users.noreply.github.com>
2021-11-24 21:04:26 +0200

    navigation bar added to game history as an example

--- app/src/main/java/com/example/darts/gamehistory/GameHistoryFragment.kt
+++ app/src/main/java/com/example/darts/gamehistory/GameHistoryFragment.kt
@@ -4,16 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.darts.R
+import com.example.darts.databinding.FragmentGameCreationBinding
+import com.example.darts.databinding.FragmentGameHistoryBinding
+
+private var _binding: FragmentGameHistoryBinding? = null
+private val binding get() = _binding!!
 
 class GameHistoryFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding: ViewDataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_history, container, false)
-
+        _binding = FragmentGameHistoryBinding.inflate(inflater, container, false)
+        binding.navigationBar.tvTitle.text = "Pelihistoria"
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /** Back to game menu fragment */
+        binding.navigationBar.btnBack.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_gameHistoryFragment_to_gameMenuFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
