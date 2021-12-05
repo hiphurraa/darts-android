0f579e0eb65abb1f90058d128ca191a4234a28cb
hiphurraa <61519166+hiphurraa@users.noreply.github.com>
2021-12-05 14:12:12 +0200

    Player creation works

--- app/src/main/java/com/example/darts/playercreation/PlayerCreationFragment.kt
+++ app/src/main/java/com/example/darts/playercreation/PlayerCreationFragment.kt
@@ -4,35 +4,74 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.darts.R
+import com.example.darts.database.DartsDatabase
+import com.example.darts.database.PlayerDao
 import com.example.darts.databinding.FragmentPlayerCreationBinding
-
-private var _binding: FragmentPlayerCreationBinding? = null
-private val binding get() = _binding!!
+import com.example.darts.gamecreation.PlayerRecyclerAdapter
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class PlayerCreationFragment: Fragment() {
+
+    private var _binding: FragmentPlayerCreationBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var database: DartsDatabase
+    private lateinit var playerDao: PlayerDao
+
+
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         _binding = FragmentPlayerCreationBinding.inflate(inflater, container, false)
-        // Setting the navigaton bar title
+
+        val context = requireActivity().applicationContext
+        database = DartsDatabase.getInstance(context)
+        playerDao = database.playerDao()
+
         binding.navigationBar.tvTitle.text = resources.getString(R.string.pc_nav_title)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Back button
+        /** Back button */
         binding.navigationBar.btnBack.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_playerCreationFragment_to_gameCreationFragment)
         }
 
-        // Continue button
+        /** Create player button */
         binding.btnContinue.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_playerCreationFragment_to_gameCreationFragment)
+
+            val playerName: String = binding.etPlayerName.text.toString()
+            val autoSelect = binding.cbAutoSelectPlayer.isChecked
+
+            val validationMsg: String = validatePlayerName(playerName)
+
+            if (validationMsg == "ok") {
+                GlobalScope.launch {
+                    playerDao.insertPlayer(playerName, autoSelect)
+                }
+                Navigation.findNavController(view).navigate(R.id.action_playerCreationFragment_to_gameCreationFragment)
+
+            } else {
+                Toast.makeText(requireActivity().applicationContext, validationMsg, Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    private fun validatePlayerName (playerName: String): String {
+        // TODO: Validate more thoroughly
+        return if (playerName.length > 20) resources.getString(R.string.pc_name_too_long)
+        else "ok"
     }
 
     override fun onDestroyView() {
